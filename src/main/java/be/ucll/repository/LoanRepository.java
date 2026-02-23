06f67a52f267cab74f051278edfcd1c240fe7095
BramVanImpeUcll <100119273+BramVanImpeUcll@users.noreply.github.com>
2026-02-23 08:47:30 +0100

    Fix loan filtering logic for active loans

--- src/main/java/be/ucll/repository/LoanRepository.java
+++ src/main/java/be/ucll/repository/LoanRepository.java
@@ -32,7 +32,7 @@ public class LoanRepository {
     public List<Loan> getLoansByUser(String email, boolean onlyActive) {
         return loans.stream()
                 .filter(loan -> loan.getUser().getEmail().equals(email))
-                .filter(loan -> !onlyActive || loan.getStartDate().isBefore(LocalDate.now()) && LocalDate.now().isAfter(loan.getEndDate()))
+                .filter(loan -> !onlyActive || loan.getStartDate().isBefore(LocalDate.now()) && LocalDate.now().isBefore(loan.getEndDate()))
                 .toList();
     }
 }
